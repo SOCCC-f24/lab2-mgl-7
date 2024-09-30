c8b6bd81ec526251d47de61116d6e50230fe08fe
mgl-7 <mgl7@sunyorange.edu>
2024-09-30 18:36:53 -0400

    Update c_cipher.py
    Changes

--- src/c_cipher.py
+++ src/c_cipher.py
@@ -19,7 +19,7 @@ def encrypt(email="abc012"):
     #     A = email[:3] (check first half)
     #     B = email[3:] (check second half)
     #     enum_flag = A or B
-    anum_flag = email[:3] != 'abc' or email[3:] != '012' 
+    anum_flag = email[:3].isalpha() != True or email[3:].isdigit() != True 
 
     if len_flag:                         # NOTE: here we provide input validation on length
         output = "Length check failed\n"
@@ -33,14 +33,17 @@ def encrypt(email="abc012"):
         return output     
         
     # TODO: fix line below, process our string into a list
-    email_lst = ["a", "b", "c", "0", "1", "2"]
+    email_lst = list(email)                #changed : ["a", "b", "c", "0", "1", "2"]
         
     # TODO: complete line(s) below, convert EACH new element into a string
-    new_ascii = ord(email_lst[0]) + 3    # NOTE: here we extract and update element at 0 
-    email_lst[0] = chr(new_ascii)        # NOTE: here we convert our ASCII into string
+    re = 0
+    while re != 6:
+        new_ascii = ord(email_lst[0+re]) + 3    # NOTE: here we extract and update element at 0 
+        email_lst[0+re] = chr(new_ascii)        # NOTE: here we convert our ASCII into string
+        re=re+1
         
     # TODO: fix line below, convert list into a string
-    email_str = "dbc012"
+    email_str = (''.join(email_lst))            #Changed: "dbc012"
 
     # keep all updates in the retVal (str) variablei
     # i.e.,
@@ -69,25 +72,31 @@ def decrypt(email="def345"):
     #     A = email[:3] (check first half)
     #     B = email[3:] (check second half)
     #     enum_flag = A or B
-    anum_flag = email[:3] != 'def' or email[3:] != '345' 
+    anum_flag = email[:3].isalpha() != True or email[3:].isdigit != True 
 
     if len_flag:                         # NOTE: here we provide input validation on length
         output = "Length check failed\n"
         output += "Email must be 6 characters long."
         logging.info(output)
         return output        
-    if anum_flag:                        # NOTE: here we provide input validation on alpha/num
+    if anum_flag != True:                        # NOTE: here we provide input validation on alpha/num
         output = "alpha num check failed\n"
         output += "Email must have 3 letters followed by 3 digits."
         logging.info(output)
         return output   
 
     # TODO: apply the encrypt pseudocode but shift down 3
-    
+    email_lst= list(email)
+    re = 0
+    while re != 6:
+        new_ascii = ord(email_lst[0+re]) - 3    # NOTE: here we extract and update element at 0 
+        email_lst[0+re] = chr(new_ascii)        # NOTE: here we convert our ASCII into string
+        re=re+1
+    email_str = (''.join(email_lst))
     # keep all updates in the retVal (str) variablei
     # i.e.,
     #    email_str = " some string updates here "
     #    email_1 = email_str.strip()
     #    retVal = email_1
-    retVal = "aef345"
+    retVal = email_str
     return retVal
